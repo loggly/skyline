024729ab555a2fb1c8373b200906b7c6dd16afd4
Mauricio Roman <mauricio@loggly.com>
2015-10-15 17:19:16 -0700

    Reduced logging

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -201,7 +201,7 @@ class Analyzer(Thread):
                 for alert in settings.ALERTS:
                     for metric in self.anomalous_metrics:
                         if alert[0] in metric[1]:
-                            logger.info("Checking if %s is in %s" % (alert[0], metric[1]))
+                            #logger.info("Checking if %s is in %s" % (alert[0], metric[1]))
                             cache_key = 'last_alert.%s.%s' % (alert[1], metric[1])
                             try:
                                 last_alert = self.redis_conn.get(cache_key)
